5b957f3a1cf38cd5765a61745b91f96cb17a462d
oharito <o.harito@gmail.com>
2015-03-14 10:24:58 +0300

    wright formatted

--- Circle.java
+++ Circle.java
@@ -2,23 +2,24 @@ import static java.lang.System.out;
 
 public class Circle {
 
-    private double radius = 0;
-    FrameCore center = new FrameCore ();
+	private double radius = 0;
+	FrameCore center = new FrameCore();
 
-    public void setRadius(double r) {
-	radius = r;
-    }
-    
-    public void setCenter (double x, double y) {
-	center.setCenter(x, y); // Fan! I can't call this method directly from class Enter (line 17)
-    }
+	public void setRadius(double r) {
+		radius = r;
+	}
 
-    public void drawCircle() { 
-	// will be a code to draw of Circle
-    }
+	public void setCenter(double x, double y) {
+		center.setCenter(x, y); // Fan! I can't call this method directly from
+								// class Enter (line 17)
+	}
 
-    public void showDataCircle() { // show data of Circle on Console
-	out.println("Центр фигуры (x0 x0) " + center.getCenterX() + "  " + center.getCenterY());
-	out.println("Радиус (radius) " + radius);
-    }
+	public void drawCircle() {
+		// will be a code to draw of Circle
+	}
+
+	public void showDataCircle() { // show data of Circle on Console
+		out.println("Центр фигуры (x0 x0) " + center.getCenterX() + "  " + center.getCenterY());
+		out.println("Радиус (radius) " + radius);
+	}
 }
--- Poly.java
+++ Poly.java
@@ -1,34 +1,33 @@
 import static java.lang.System.out;
 
-
 public class Poly { // class of Poly Line
-    private int vertex;
-    private double[][] points;
-    
-    public void setVertex (int v) {
-	vertex = v;
-	points = new double[vertex][2];
-    }
-    
-    public int getVertex () {
-	return vertex;
-    }
-    
-    public void setPoint (int i, int j, double z) {
-	points [i][j] = z;
-    }
+	private int vertex;
+	private double[][] points;
 
-    public void drawPoly() { // draw of Poly Line
-	// will be a code to draw of Poly Line
-    }
+	public void setVertex(int v) {
+		vertex = v;
+		points = new double[vertex][2];
+	}
 
-    public void showDataPoly() { // show data of Poly Line on Console
-	for (int x = 0; x < vertex; x++) {
-		out.print("Точка " + x + " = ");
-		for (int y = 0; y < 2; y++) {
-		    out.print(points[x][y] + "  ");
+	public int getVertex() {
+		return vertex;
+	}
+
+	public void setPoint(int i, int j, double z) {
+		points[i][j] = z;
+	}
+
+	public void drawPoly() { // draw of Poly Line
+		// will be a code to draw of Poly Line
+	}
+
+	public void showDataPoly() { // show data of Poly Line on Console
+		for (int x = 0; x < vertex; x++) {
+			out.print("Точка " + x + " = ");
+			for (int y = 0; y < 2; y++) {
+				out.print(points[x][y] + "  ");
+			}
+			out.println();
 		}
-		out.println();
-	    }
-    }
+	}
 }
