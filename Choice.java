56aaa5dcc602ab3a3351dbf74adf2cae7c6c9068
oharito <o.harito@gmail.com>
2015-03-17 19:34:45 +0300

    Ver 4.7

--- Choice.java
+++ Choice.java
@@ -1,9 +1,11 @@
 public class Choice {
+    	public String list = "Input Type of Shape:\n"
+    		+ " 1 - Circle\n "
+    		+ "2 - Equilateral polygon\n "
+    		+ "3 - PolyLine";
     
-    Shape choice(int k) {
-	
+    public Shape choice (int k) {
 	switch (k) {
-	
 	case 1:
 	    return new Circle();
 	case 2:
--- drawOfFigure.java
+++ drawOfFigure.java
@@ -3,15 +3,10 @@ import javax.swing.JOptionPane;
 public class drawOfFigure {
     public static void main(String[] args) {
 	int kind;
-	kind = Integer
-		    .parseInt(JOptionPane
-			    .showInputDialog("Input Type of Shape:\n 1 - Circle\n 2 - Equilateral polygon\n 3 - PolyLine"));
-	System.out.println("Type of the Shape :  " + kind);
 	Choice choice = new Choice();
+	kind = Integer.parseInt(JOptionPane.showInputDialog(choice.list));
+	System.out.println("Type of the Shape :  " + kind);
+//	Choice choice = new Choice();
 	choice.choice(kind);
-	
-//	in.setKind();
-//	in.choice();
-	
     }
 }
