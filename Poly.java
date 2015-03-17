a4a68b849b075dca068905bd6859981b9fe5eda8
oharito <o.harito@gmail.com>
2015-03-17 13:37:42 +0300

    Ver 4.2

--- Poly.java
+++ Poly.java
@@ -1,19 +1,19 @@
 import javax.swing.JOptionPane;
+import java.util.ArrayList;
 
 public class Poly extends Shape { // class of PolyLine
     private int vertex;
-    Point2d [] dot;
+    private ArrayList <Point2d> dot = new ArrayList <Point2d>();
 
     public void setData() {
 	int v = Integer.parseInt(JOptionPane.showInputDialog("Введите количество вершин:"));
 	vertex = v;
-	dot = new Point2d [vertex];
 	double x;
 	double y;
 	for (int i = 0; i < vertex; i++) {
 	    x = Double.parseDouble(JOptionPane.showInputDialog("Введите координаты X" + i + " фигуры:"));
 	    y = Double.parseDouble(JOptionPane.showInputDialog("Введите координаты Y" + i + " фигуры:"));
-	    dot [i] = new Point2d (x,y);
+	    dot.add(new Point2d (x,y));
 	}
 	drawShape();
     }
@@ -21,7 +21,7 @@ public class Poly extends Shape { // class of PolyLine
     public void drawShape() { // it's only show the data on console
 	for (int i = 0; i < vertex; i++) {
 	    System.out.print("Точка " + i + "  =  ");
-	    System.out.println (dot[i].getCenterX() + "   " + dot[i].getCenterY());
+	    System.out.println (dot.get(i).getCenterX() + "   " + dot.get(i).getCenterY());
 	    }
     }
 
