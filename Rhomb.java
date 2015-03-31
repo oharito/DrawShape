b285e3448371686dd6f5e3468fefd6d78c78a718
oharito <o.harito@gmail.com>
2015-03-31 17:58:07 +0300

    13.03.15 18:00

--- Rhomb.java
+++ Rhomb.java
@@ -1,7 +1,3 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -9,26 +5,26 @@ public class Rhomb implements Shape { // class of Rhomb
     private double width;
     private double height;
     Point2d center;
-    private Scanner readFile;
-    private PrintStream fileTemp;
+    private String data;
+    private String [] tokens;
 
-    Rhomb() throws FileNotFoundException { // constructor
-	inputAndSetCenter();
-	// double x = Double.parseDouble(JOptionPane.showInputDialog("Input Center's coordinate X:"));
-	// double y = Double.parseDouble(JOptionPane.showInputDialog("Input Center's coordinate Y:"));
-	// center = new Point2d(x, y);
-	width = Double.parseDouble(JOptionPane.showInputDialog("Input Rhomb's width:"));
-	height = Double.parseDouble(JOptionPane.showInputDialog("Input Rhomb's height:"));
+    Rhomb() { // constructor
+	inputData();
+	setData();
 	drawShape();
     }
-
-    void inputAndSetCenter() throws FileNotFoundException {
-	// String s;
-	String s = JOptionPane.showInputDialog("Input Center's coordinate X0 Y0:");
-	fileTemp = new PrintStream("Temp");
-	fileTemp.println(s);
-	readFile = new Scanner(new File("Temp"));
-	center = new Point2d(readFile.nextDouble(), readFile.nextDouble());
+    
+    void inputData () {
+	data = JOptionPane.showInputDialog("Input via blank: \"Center's coordinate X0\" \"Y0\" \"Rhomb's Width\" \"Height:\"");
+	tokens = data.split(" ");
+    }
+    
+    void setData () {
+	double x = Double.parseDouble(tokens[0]);
+	double y = Double.parseDouble(tokens[1]);
+	center = new Point2d(x, y);
+	width = Double.parseDouble(tokens[2]);
+	height = Double.parseDouble(tokens[3]);
     }
 
     Rhomb(double width, double height, Point2d center) { // Constructor for the class Test
