f86decb687c44bea77f284c530703d170198b76a
oharito <o.harito@gmail.com>
2015-04-20 15:39:27 +0300

    Refactoring class Input

--- Input.java
+++ Input.java
@@ -2,11 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+//import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 public class Input {
     private static String data;
+    private static String file;
     private static String [] tokens;
     
     public static void input(String order) {
@@ -16,7 +19,17 @@ public class Input {
     public static void inputDataFromFile() {
 	Scanner readFile = null;
 	    try {
-		readFile = new Scanner (new File("shape.txt"));
+		JFileChooser chooser = new JFileChooser("E:\\");
+//		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+//		        "*.txt", "jpg", "gif");
+//		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(chooser);
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+		   System.out.println("You chose to open this file: " +
+		        chooser.getSelectedFile().getName());
+		   file = chooser.getSelectedFile().getName();
+		}
+		readFile = new Scanner (new File("E:\\"+file));
 	    } catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
